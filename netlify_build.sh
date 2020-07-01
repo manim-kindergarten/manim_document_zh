8e89c83f91790716b0d57f3c7743aa8d0fa0c454
Tony031218 <975062472@qq.com>
2020-07-01 08:09:49 +0800

    update

--- netlify_build.sh
+++ netlify_build.sh
@@ -4,32 +4,35 @@ cd manim
 git clone https://github.com/manim-kindergarten/manim_sandbox.git
 cd ../repo
 
-mkdir /usr/lib/python3.5/site-packages/
+# mkdir /usr/lib/python3.5/site-packages/
 
-wget --no-check-certificate  https://pypi.python.org/packages/source/s/setuptools/setuptools-19.6.tar.gz#md5=c607dd118eae682c44ed146367a17e26
-tar -zxvf setuptools-19.6.tar.gz
-cd setuptools-19.6
-python3 setup.py build
-python3 setup.py install
-cd ..
+# wget --no-check-certificate  https://pypi.python.org/packages/source/s/setuptools/setuptools-19.6.tar.gz#md5=c607dd118eae682c44ed146367a17e26
+# tar -zxvf setuptools-19.6.tar.gz
+# cd setuptools-19.6
+# python3 setup.py build
+# python3 setup.py install
+# cd ..
 
-wget --no-check-certificate https://pypi.python.org/packages/source/p/pip/pip-18.1.tar.gz
-tar -zxvf pip-18.1.tar.gz 
-cd pip-18.1
-python3 setup.py build
-python3 setup.py install
-cd ..
-python3 -m pip --version
+# wget --no-check-certificate https://pypi.python.org/packages/source/p/pip/pip-18.1.tar.gz
+# tar -zxvf pip-18.1.tar.gz 
+# cd pip-18.1
+# python3 setup.py build
+# python3 setup.py install
+# cd ..
+# python3 -m pip --version
+
+python --version
+python -m pip --version
 
 cd ..
 git clone https://github.com/sphinx-doc/sphinx
 cd sphinx
-python3 -m pip install .
-python3 -m pip install sphinx_rtd_theme
-python3 -m pip install jieba
+python -m pip install .
+python -m pip install sphinx_rtd_theme
+python -m pip install jieba
 cd ../repo
 
 cd ../manim
-python3 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 make html
