81b01bbba99d2c6d51e964ce44a6601f61fb530f
Tony031218 <975062472@qq.com>
2020-07-01 10:29:55 +0800

    add requirements.txt

--- netlify_build.sh
+++ netlify_build.sh
@@ -2,21 +2,12 @@ cd ..
 git clone -b manim https://github.com/manim-kindergarten/manim_document_zh.git manim
 cd manim
 git clone https://github.com/manim-kindergarten/manim_sandbox.git
-cd ../repo
-
-python --version
-python -m pip --version
 
 cd ..
+python -m pip uninstall sphinx -y
 git clone https://github.com/sphinx-doc/sphinx
 cd sphinx
 python -m pip install .
-python -m pip install sphinx_rtd_theme
-python -m pip install jieba
-cd ../repo
-
-cd ../manim
-python -m pip install -r requirements.txt
 cd ../repo
 
 make html
