97cbed0910c1e9903183556da39e5de76c450e1e
TonyCrane <975062472@qq.com>
2021-06-13 11:10:35 +0800

    update script for netlify

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,14 +1,13 @@
 cd ..
-git clone -b manim https://github.com/manim-kindergarten/manim_document_zh.git manim
-cd manim
+git clone -b manim https://github.com/manim-kindergarten/manim_document_zh.git manim_with_doc
+cd manim_with_doc
 git clone https://github.com/manim-kindergarten/manim_sandbox.git
 
-cd ..
-/opt/buildhome/python3.7/bin/python3.7 -m pip install --upgrade pip
-python -m pip uninstall sphinx -y
-git clone https://github.com/sphinx-doc/sphinx
-cd sphinx
-python -m pip install .
-cd ../repo
+cd repo
 
 make html
+
+cd shaders
+make html
+mv build/html ../build/html/shaders
+cd ..
