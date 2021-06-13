75304c730670df489c51423039dfee8d5730ac95
TonyCrane <975062472@qq.com>
2021-06-13 11:34:50 +0800

    clean netlify_build.sh

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,14 +1,16 @@
+# Clone pre repositries and Install python requirements
 cd ..
 git clone -b manim https://github.com/manim-kindergarten/manim_document_zh.git manim_with_doc
 cd manim_with_doc
 git clone https://github.com/manim-kindergarten/manim_sandbox.git
-python -m pip --version
+python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
-pwd
 cd ../repo
-# export PATH="$PATH:/home/runner/.local/bin:/home/runner/manim_document_zh/manim_with_doc"
+
+# Build doc
 make html
 
+# Build doc for shaders
 cd shaders
 make html
 mv build/html ../build/html/shaders
