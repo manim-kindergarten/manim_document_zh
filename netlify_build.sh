3c2ef607847b09b56715ace796a743ecdbfeee96
TonyCrane <975062472@qq.com>
2021-06-13 11:27:11 +0800

    add debug scrip

--- netlify_build.sh
+++ netlify_build.sh
@@ -2,9 +2,11 @@ cd ..
 git clone -b manim https://github.com/manim-kindergarten/manim_document_zh.git manim_with_doc
 cd manim_with_doc
 git clone https://github.com/manim-kindergarten/manim_sandbox.git
-
+python -m pip --version
+python -m pip install -r requirements.txt
+pwd
 cd ../repo
-
+# export PATH="$PATH:/home/runner/.local/bin:/home/runner/manim_document_zh/manim_with_doc"
 make html
 
 cd shaders
