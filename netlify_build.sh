378b2fa6c7cd8fc4de15b36aca519d83f3228dad
TonyCrane <975062472@qq.com>
2021-10-18 09:15:01 +0800

    fix netlify

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,17 +1,24 @@
 # Clone pre repositries and Install python requirements
-cd ..
 git clone -b manim https://github.com/manim-kindergarten/manim_document_zh.git manim_with_doc
 cd manim_with_doc
 git clone https://github.com/manim-kindergarten/manim_sandbox.git
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
-cd ../repo
+cd ..
 
 # Build doc
+cd cairo-backend
 make html
+mkdir -p ../build/html/cairo-backend
+mv build/html/* ../build/html/cairo-backend/
 
 # Build doc for shaders
-cd shaders
+cd ..
+git clone https://github.com/manim-kindergarten/manim.git manim_cn 
+cd manim_cn 
+pip install .
+pip install -r docs/requirements.txt 
+cd docs
 make html
-mv build/html ../build/html/shaders
+mv build/html/* ../../build/html/
 cd ..
